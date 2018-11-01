ad3974b3db66502ff46c2c75320c4ec704c4e832
TangAnna <atang@singlemountaintech.com>
2018-11-01 15:46:55 +0800

    添加gradle配置服务器不同地址

--- app/src/main/java/tanganna/com/umengandroidapk/MainActivity.java
+++ app/src/main/java/tanganna/com/umengandroidapk/MainActivity.java
@@ -1,13 +1,17 @@
 package tanganna.com.umengandroidapk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.tv_main);
+        textView.setText(BuildConfig.API_URL);
     }
 }
